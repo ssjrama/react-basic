cbf9c9f605a3a17fda45432835d7e2efa2555931
Wahyu Ramadhan <ssjrama@gmail.com>
2021-01-15 13:00:37 +0700

    Initial Commit

---
+++ src/Book.js
@@ -0,0 +1,35 @@
+const Book = (props) => {
+  const { img, title, author } = props;
+
+  //attribute, eventHandler
+  const clickHandler = (e) => {
+    console.log(e);
+    console.log(e.target);
+    alert("hello world");
+  };
+
+  const complexExample = (author) => {
+    console.log(author);
+  };
+
+  return (
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(title);
+      }}
+    >
+      <img src={img} alt="" />
+      <h1 onClick={() => console.log(title)}>{title}</h1>
+      <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>
+        reference example
+      </button>
+      <button type="button" onClick={() => complexExample(author)}>
+        more complex example
+      </button>
+    </article>
+  );
+};
+
+export default Book;
---
+++ src/books.js
@@ -0,0 +1,24 @@
+//Setuo Vars
+export const books = [
+  {
+    id: 1,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/71kxa1-0mfL._AC_UL200_SR200,200_.jpg",
+    title: "1984 (Signet Classics)",
+    author: "George Orwell",
+  },
+  {
+    id: 2,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/91AQs6qv9ML._AC_UL200_SR200,200_.jpg",
+    title: "Untamed",
+    author: "Glennon Doyle",
+  },
+  {
+    id: 3,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81PxN0B7CmL._AC_UL200_SR200,200_.jpg",
+    title: "Magnolia Table",
+    author: "Joanna Gaines",
+  },
+];
